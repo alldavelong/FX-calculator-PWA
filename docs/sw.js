5ec7cf7d8aba278d3124d28713a4406fdd875644
David Ebner <2010653205@stud.fh-kufstein.ac.at>
2021-12-09 22:36:37 +0100

    first commit
    - contains html file, css stylesheet and app.js for currency exchange calculation
    - contains webmanifest
    - implemented serviceworker

---
+++ docs/sw.js
@@ -0,0 +1,29 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('fxcalc-v1')
+        .then(cache => cache.addAll([
+            './',
+            './index.html',
+            './app.js',
+            './icon512.png',
+            './maskable-icon512.png',
+            './icon192.png',
+            './icon16.png',
+            './fixer.json',
+            './manifest.webmanifest'
+        ]))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.open('fxcalc-v1').then((cache) => {
+            return fetch(event.request) // request at the network
+                .then((response) => { // request successful (online)
+                    cache.put(event.request, response.clone());
+                    return response;
+                })
+                .catch(() => cache.match(event.request)); // request not deliverable to network (offline)
+        })
+    );
+});
